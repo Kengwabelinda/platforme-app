056127c493d348547964584ac5892ac74c538e81
Kengwabelinda <kengwawatkobelinda@gmail.com>
2024-07-09 16:03:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php 
+//scalar types(contain one value)
+$string = "dani krossing";
+$int = 123445;
+$float = 2.5678;
+$bool = true;
+
+//array types
+$names = ["daniel", "bella", "frida"];
+
+ 
+?> 
+ 
+</body>
+</html>
